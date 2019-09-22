87c5678222a21ee945ff54f28972e29687fdae82
yao0510 <sf163864@gmail.com>
2019-09-22 17:04:24 +0800

    finish hw1

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -9,7 +9,7 @@ np.set_printoptions(precision=5, suppress=True)
 
 def matrix_A(x,base):
     A = np.zeros((len(x), base))
-    print(np.shape(A))
+    # print(np.shape(A))
     for value_idx in range(len(x)):
         for base_idx in range(base-1,-1,-1):
             A[value_idx][base-base_idx-1] = x[value_idx]**base_idx
@@ -18,8 +18,6 @@ def matrix_A(x,base):
 
 def LU_decomposition(A):
     row_size,col_size = np.shape(A)
-    print(row_size) 
-    print(col_size)
     L = np.identity(row_size)
     for row in range(row_size):
         L_tmp = np.identity(row_size)
@@ -64,29 +62,29 @@ def print_result(A,x,y,ori_x,model):
             if row != 0:                   #check first
                 if x[row] >= 0:            #check value
                     print("+", end=' ')
-                    print(str(x[row]) + ' X^' + str((np.shape(x)[0]-1) - row), end=' ')
+                    print(str(x[row][0]) + ' X^' + str((np.shape(x)[0]-1) - row), end=' ')
                 else:
                     print("-", end=' ')
-                    print(str((-1) * x[row]) + ' X^' + str((np.shape(x)[0]-1) - row), end=' ')
+                    print(str((-1) * x[row][0]) + ' X^' + str((np.shape(x)[0]-1) - row), end=' ')
             else:
                 if x[row] >= 0:
-                    print(str(x[row]) + ' X^' + str((np.shape(x)[0]-1) - row), end=' ')
+                    print(str(x[row][0]) + ' X^' + str((np.shape(x)[0]-1) - row), end=' ')
                 else:
                     print("-", end=' ')
-                    print(str((-1) * x[row]) + ' X^' + str((np.shape(x)[0]-1) - row), end=' ')
+                    print(str((-1) * x[row][0]) + ' X^' + str((np.shape(x)[0]-1) - row), end=' ')
         else:
             if x[row] >= 0:
                 print("+", end=' ')
-                print(str(x[row]))
+                print(str(x[row][0]))
             else:
                 print("-", end=' ')
-                print(str((-1) * x[row]))
+                print(str((-1) * x[row][0]))
 
     # Calculate total error
     for row in range(np.shape(cal_b)[0]):
         tmp_error = (cal_b[row] - y[row]) ** 2
         total_error += tmp_error
-    print("Total error: ",total_error)
+    print("Total error: ",total_error[0])
 
     fig = plt.figure()
     plt.title(model)
@@ -94,6 +92,15 @@ def print_result(A,x,y,ori_x,model):
     plt.plot(ori_x,cal_b)
     # plt.xlim((min(ori_x), max(ori_x)))
     fig.savefig(model + '.png')
+
+def getGradient(A_transpose_A,x,A_transpose_b):
+    A_transpose_b_2 = 2 * A_transpose_b                     #2 A^T b
+    A_transpose_A_x_2 = 2 * (np.matmul(A_transpose_A,x))
+    gradient = np.subtract(A_transpose_A_x_2,A_transpose_b_2)
+    return gradient
+
+def getHession(A_transpose_A):
+    return 2 * A_transpose_A
 
 if __name__ == '__main__':
     #parse parameters
@@ -112,22 +119,38 @@ if __name__ == '__main__':
             point.append([float(row[0]),float(row[1])])
     point = np.array(point)
 
-    #Rlse
-    print("LSE:")
     ori_x = point[:,0]
-    b = point[:,1]
+    b = point[:,1].reshape((len(point[:,1]),1))
     A = matrix_A(point[:,0],N)
-    grim_matrix = np.matmul(A.T,A)
-    lambda_I = LAMBDA*np.identity(np.shape(grim_matrix)[0])
-    grim_matrix_add_lambda_I = np.add(grim_matrix,lambda_I)
-
-    # test_matrix2 = np.array([[2,1,3,2],[0,3,-2,1],[2,1,-1,1],[2,1,2,2]])
-    # test_b = np.array([1,3,2,4])
-    # L,U = LU_decomposition(test_matrix2)
-    # result = substitution(L,U,test_b)
-
-    L,U = LU_decomposition(grim_matrix_add_lambda_I)
-    
     A_transpose_b = np.matmul(A.T,b)
-    result = substitution(L,U,A_transpose_b)
-    print_result(A,result,b,ori_x,'LSE')
+    gram_matrix = np.matmul(A.T,A)      #A^T * A
+
+    """
+    test_matrix2 = np.array([[2,1,3,2],[0,3,-2,1],[2,1,-1,1],[2,1,2,2]])
+    test_b = np.array([1,3,2,4])
+    L,U = LU_decomposition(test_matrix2)
+    result = substitution(L,U,test_b)
+    """
+
+    #Rlse
+    lambda_I = LAMBDA * np.identity(np.shape(gram_matrix)[0])
+    gram_matrix_add_lambda_I = np.add(gram_matrix,lambda_I)
+    L,U = LU_decomposition(gram_matrix_add_lambda_I)
+    lse_result = substitution(L,U,A_transpose_b)
+    print_result(A,lse_result,b,ori_x,'LSE')
+
+    print("=============================")
+
+    #Newton's method
+    newton_guess = np.zeros((N ,1), dtype=float)
+    A_transpose_b = np.matmul(A.T,b)            # A^T b will change in substitution,so weird
+    gradient = getGradient(gram_matrix,newton_guess,A_transpose_b)
+    hession = getHession(gram_matrix)
+    L,U = LU_decomposition(hession)
+    hession_inv = np.zeros((N ,N), dtype=float)
+    I_matrix = np.identity(N)
+    for col in range(N):
+        hession_inv[:,col] = substitution(L,U,I_matrix[:,col])
+    newton_result = np.subtract(newton_guess, np.matmul(hession_inv,gradient))
+    # print("final result = ",newton_result)
+    print_result(A,newton_result,b,ori_x,"Newton's method")

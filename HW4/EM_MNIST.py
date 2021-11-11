4a00a66ec7ce387bfab2372113689b9a8e561ca7
yao0510 <sf163864@gmail.com>
2021-11-11 15:11:00 +0800

    set seed to reproduce and modify style

--- HW4/EM_MNIST.py
+++ HW4/EM_MNIST.py
@@ -7,32 +7,33 @@ np.set_printoptions(precision=5, suppress=True)
 
 # py EM_algorithm.py
 
+SEED = 42
+np.random.seed(SEED)
+
 # @nb.jit
 def open_file():
 	data_type = np.dtype("int32").newbyteorder('>')
 	
-	data = np.fromfile("./train-images.idx3-ubyte", dtype = "ubyte")
-	train_image = data[4 * data_type.itemsize:].astype("float64").reshape(60000, 28 * 28)
+	data = np.fromfile("./train-images.idx3-ubyte", dtype="ubyte")
+	train_image = data[4 * data_type.itemsize:].astype("float64").reshape(60000, 28*28)
 	train_image_bin = np.divide(train_image, 128).astype("int")
 
-	train_label = np.fromfile("./train-labels.idx1-ubyte",dtype = "ubyte").astype("int")
-	train_label = train_label[2 * data_type.itemsize : ].reshape(60000, 1)
+	train_label = np.fromfile("./train-labels.idx1-ubyte", dtype="ubyte").astype("int")
+	train_label = train_label[2 * data_type.itemsize:].reshape(60000, 1)
 	return train_image_bin, train_label
 
 @nb.jit
-def Estep(train_bin, MU, PI, Z):
+def Estep(train_bin, mu, pi, Z):
     # E step
     for img_idx in range(img_cnt):
-#         if img_idx % 10000 == 0:
-#             print("Status : ", img_idx)
         num_sum = np.full((num_cnt, 1), 1, dtype=np.float64)
         for num_idx in range(num_cnt):
             for pixel_idx in range(pixel_cnt):
                 if train_bin[img_idx][pixel_idx] == 1:
-                    num_sum[num_idx][0] *= MU[num_idx][pixel_idx]
+                    num_sum[num_idx][0] *= mu[num_idx][pixel_idx]
                 else:
-                    num_sum[num_idx][0] *= (1 - MU[num_idx][pixel_idx])
-            num_sum[num_idx][0] *= PI[num_idx][0]
+                    num_sum[num_idx][0] *= (1-mu[num_idx][pixel_idx])
+            num_sum[num_idx][0] *= pi[num_idx][0]
 
         marginal_num = np.sum(num_sum)
         if marginal_num == 0:
@@ -44,7 +45,7 @@ def Estep(train_bin, MU, PI, Z):
     return Z
 
 # @nb.jit
-def Mstep(train_bin, MU, PI, Z):
+def Mstep(train_bin, mu, pi, Z):
     N_cluster = np.sum(Z, axis=0)
     for num_idx in range(num_cnt):
         for pixel_idx in range(pixel_cnt):
@@ -52,48 +53,48 @@ def Mstep(train_bin, MU, PI, Z):
             marginal = N_cluster[num_idx]
             if marginal == 0:
                 marginal = 1
-            MU[num_idx][pixel_idx] = sum_pixel / marginal
+            mu[num_idx][pixel_idx] = sum_pixel / marginal
 
-        PI[num_idx][0] = N_cluster[num_idx] / img_cnt
-        if PI[num_idx][0] == 0:
-            PI[num_idx][0] = 1
+        pi[num_idx][0] = N_cluster[num_idx] / img_cnt
+        if pi[num_idx][0] == 0:
+            pi[num_idx][0] = 1
 
-    return MU, PI
+    return mu, pi
 
 @nb.jit
-def difference(MU, MU_prev):
+def difference(mu, mu_prev):
     diff = 0
     for num_idx in range(num_cnt):
         for pixel_idx in range(pixel_cnt):
-            diff += abs(MU[num_idx][pixel_idx] - MU_prev[num_idx][pixel_idx])
+            diff += abs(mu[num_idx][pixel_idx] - mu_prev[num_idx][pixel_idx])
     return diff
 
 # @nb.jit
-def print_imagination(MU):
-    MU_new = MU.copy()
+def print_imagination(mu):
+    mu_new = mu.copy()
     for num_idx in range(num_cnt):
         print("\nclass: ", num_idx)
         for pixel_idx in range(pixel_cnt):
             if pixel_idx % 28 == 0 and pixel_idx != 0:
                 print("")
-            if MU_new[num_idx][pixel_idx] >= 0.5:
+            if mu_new[num_idx][pixel_idx] >= 0.5:
                 print("1", end=" ")
             else:
                 print("0", end=" ")
         print("")
 
 @nb.jit
-def label_cluster(train_bin, train_label, MU, PI):
+def label_cluster(train_bin, train_label, mu, pi):
     table = np.zeros(shape=(num_cnt, num_cnt), dtype=np.int)
     for img_idx in range(img_cnt):
         num_sum = np.full((num_cnt), 1, dtype=np.float64)
         for num_idx in range(num_cnt):
             for pixel_idx in range(pixel_cnt):
                 if train_bin[img_idx][pixel_idx] == 1:
-                    num_sum[num_idx] *= MU[num_idx][pixel_idx]
+                    num_sum[num_idx] *= mu[num_idx][pixel_idx]
                 else:
-                    num_sum[num_idx] *= (1 - MU[num_idx][pixel_idx])
-            num_sum[num_idx] *= PI[num_idx][0]
+                    num_sum[num_idx] *= (1 - mu[num_idx][pixel_idx])
+            num_sum[num_idx] *= pi[num_idx][0]
         table[train_label[img_idx][0]][np.argmax(num_sum)] += 1
 
     relation = np.full((num_cnt), -1, dtype=np.int)
@@ -107,21 +108,21 @@ def label_cluster(train_bin, train_label, MU, PI):
 
     return relation
 
-def print_label(relation, MU):
+def print_label(relation, mu):
     for num_idx in range(num_cnt):
         cluster = relation[num_idx]
         print("\nLabeled class : ", num_idx)
         for pixel_idx in range(pixel_cnt):
             if pixel_idx % 28 == 0 and pixel_idx != 0:
                 print("")
-            if MU[cluster][pixel_idx] >= 0.5:
+            if mu[cluster][pixel_idx] >= 0.5:
                 print("1", end=" ")
             else:
                 print("0", end=" ")
         print("")
 
 @nb.jit
-def print_confusion_matrix(train_bin, train_label, MU, PI, relation):
+def print_confusion_matrix(train_bin, train_label, mu, pi, relation):
     error = img_cnt
     confusion_matrix = np.zeros(shape=(num_cnt, 4), dtype=np.int)       # TP FP TN FN
     for img_idx in range(img_cnt):
@@ -129,15 +130,13 @@ def print_confusion_matrix(train_bin, train_label, MU, PI, relation):
         for num_idx in range(num_cnt):
             for pixel_idx in range(pixel_cnt):
                 if train_bin[img_idx][pixel_idx] == 1:
-                    num_sum[num_idx] *= MU[num_idx][pixel_idx]
+                    num_sum[num_idx] *= mu[num_idx][pixel_idx]
                 else:
-                    num_sum[num_idx] *= (1 - MU[num_idx][pixel_idx])
-            num_sum[num_idx] *= PI[num_idx][0]
+                    num_sum[num_idx] *= (1 - mu[num_idx][pixel_idx])
+            num_sum[num_idx] *= pi[num_idx][0]
 
         predict_cluster = np.argmax(num_sum)
         predict_label = np.where(relation==predict_cluster)
-        # print("cluster = {}".format(predict_cluster))
-        # print("label = {}".format(predict_label))
 
         for num_idx in range(num_cnt):
             if num_idx == train_label[img_idx][0]:
@@ -173,26 +172,26 @@ if __name__ == '__main__':
 
     print("train bin size = {}".format(train_bin.shape))
 
-    PI = np.random.random_sample((num_cnt, 1))
-    MU = np.random.random_sample((num_cnt, pixel_cnt))
-    MU_prev = np.zeros((num_cnt, pixel_cnt), dtype=np.float64)
+    pi = np.random.random_sample((num_cnt, 1))
+    mu = np.random.random_sample((num_cnt, pixel_cnt))
+    mu_prev = np.zeros((num_cnt, pixel_cnt), dtype=np.float64)
     Z  = np.random.random_sample((img_cnt, 10)) 
 
     for step in range(epochs):
-        Z = Estep(train_bin, MU, PI, Z)
-        MU, PI = Mstep(train_bin, MU, PI, Z)
-        gap = difference(MU, MU_prev)
+        Z = Estep(train_bin, mu, pi, Z)
+        mu, pi = Mstep(train_bin, mu, pi, Z)
+        gap = difference(mu, mu_prev)
 
-        print_imagination(MU)
+        print_imagination(mu)
         print("No. of Iteration: {}, Difference: {}\n".format(step+1, gap))
         print("---------------------------------------------------------------\n")
 
-        MU_prev = MU
+        mu_prev = mu.copy()
 
-    relation = label_cluster(train_bin, train_label, MU, PI)
+    relation = label_cluster(train_bin, train_label, mu, pi)
     print(relation)
-    print_label(relation, MU)
+    print_label(relation, mu)
     print("---------------------------------------------------------------\n")
-    error = print_confusion_matrix(train_bin, train_label, MU, PI, relation)
+    error = print_confusion_matrix(train_bin, train_label, mu, pi, relation)
     print("Total iteration to converge: {}".format(epochs))
     print("Total error rate: {}".format(error / img_cnt))
